eaae9bc2228b47f0e0852cc06b867e345ce8c5e0
Clive Norman <cjn@hazlegrove.co.uk>
2021-03-22 15:38:08 +0000

    Added LinkTree

--- docs/sw.js
+++ docs/sw.js
@@ -2,7 +2,7 @@
   "use strict";
 
   // Update 'version' if you need to refresh the cache
-  var version = "NS22032021V23::CacheFirstSafe";
+  var version = "NS22032021V24::CacheFirstSafe";
   var offlineUrl = "/offline.html";
 
   // Store core files in a cache (including a page to display when offline)
